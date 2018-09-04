119e6974bd24154b17e57858cdf7053c0118ec36
Namek <kamil@namekdev.net>
2018-09-04 20:25:42 +0200

    frontend: remove Ports

--- FundshareFrontend/src/elm/src/Ports.elm
+++
@@ -1,9 +0,0 @@
-port module Ports exposing (onSessionChange)
-
-import Json.Encode exposing (Value)
-
-
--- port storeToken : Maybe String -> Cmd msg
-
-
-port onSessionChange : (Value -> msg) -> Sub msg
--- FundshareFrontend/src/static/js/app.js
+++ FundshareFrontend/src/static/js/app.js
@@ -1,10 +1,1 @@
 const app = Elm.Main.embed(document.getElementById("elm-main"), localStorage.sessionToken || null);
-
-app.ports.storeToken.subscribe(function(token) {
-    localStorage.sessionToken = token;
-});
-window.addEventListener("storage", function(event) {
-    if (event.storageArea === localStorage && event.key === "session") {
-        app.ports.onSessionChange.send(event.newValue);
-    }
-}, false);
