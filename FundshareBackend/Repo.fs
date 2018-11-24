896125e969d8b8a1f082f64f62df7f03711da63f
Namek <kamil@namekdev.net>
2018-11-24 03:20:11 +0100

    backend: at least one beneficient has to be specified

--- FundshareBackend/Repo.fs
+++ FundshareBackend/Repo.fs
@@ -314,20 +314,27 @@ let getAllUsers() : User list =
 let addTransaction (args : Input_AddTransaction) : Result<UserTransaction, String> =
   let now = DateTime.Now
   let acceptanceIds = [args.authorId]
-  connect()
-  |> Sql.executeQueries
-    [ ScalarQuery (
-        "INSERT INTO public.transactions
-           (amount, author_id, payor_id, beneficient_ids, acceptance_ids, tags, description, inserted_at, updated_at)
-         VALUES (@amount, @aid, @pid, @bids, @aids, @tags, @descr, @timeNow, @timeNow) RETURNING id",
-        [ "amount", QInt args.amount
-          "aid", QInt args.authorId
-          "pid", QInt args.payorId
-          "bids", QIntArray args.beneficientIds
-          "aids", QIntArray acceptanceIds
-          "tags", QStringArray args.tags
-          "descr", args.description |> Option.map QString |> Option.defaultWith (fun () -> QNull)
-          "timeNow", QDate <| now ] ) ]
+
+  ( if List.isEmpty args.beneficientIds then
+      Error "At least one beneficient has to be specified."
+    else
+      Ok true )
+  |> Result.bind (fun _ ->
+    connect()
+    |> Sql.executeQueries
+      [ ScalarQuery (
+          "INSERT INTO public.transactions
+             (amount, author_id, payor_id, beneficient_ids, acceptance_ids, tags, description, inserted_at, updated_at)
+           VALUES (@amount, @aid, @pid, @bids, @aids, @tags, @descr, @timeNow, @timeNow) RETURNING id",
+          [ "amount", QInt args.amount
+            "aid", QInt args.authorId
+            "pid", QInt args.payorId
+            "bids", QIntArray args.beneficientIds
+            "aids", QIntArray acceptanceIds
+            "tags", QStringArray args.tags
+            "descr", args.description |> Option.map QString |> Option.defaultWith (fun () -> QNull)
+            "timeNow", QDate <| now ] ) ]
+  )
   |> function
     | Ok [ScalarResult (QInt transactionId) ] ->
         { id = transactionId
